e8f0ba0e3c31a5a47a5199af8dd97c9a0c975de2
kubragzc <149508511+kubragzc@users.noreply.github.com>
2025-02-06 20:21:08 +0300

    Update meryem.py

--- meryem.py
+++ meryem.py
@@ -1,1 +1,25 @@
-kodlarrrr
+import discord
+from bot_mantik import gen_pass
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hoşgeldin'):
+        await message.channel.send("Selam!, naber ? ")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("yine bekleriz")
+    elif message.content.startswith('$parola'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send(message.content)
